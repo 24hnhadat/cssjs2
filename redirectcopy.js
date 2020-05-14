cf8899dfe7735241f682756d0da13fe497152de7
24hnhadat <65288381+24hnhadat@users.noreply.github.com>
2020-05-14 10:17:32 +0700

    Update redirectcopy.js

--- redirectcopy.js
+++ redirectcopy.js
@@ -2,7 +2,7 @@ function FSAddOriginalLink(){
     var body_element = document.getElementsByTagName('body')[0];
     var selection;
     selection = window.getSelection();
-    var pagelink = "<br/><br/>MUON COPY THI VAO DAY : <a href='https://bluestarquan7.blogspot.com/'>https://bluestarquan7.blogspot.com/"+document.location.pathname+"</a>";
+    var pagelink = "<br/><br/>MUON COPY THI VAO DAY : <a href='http://www.24hnhadat.vn/'>http://www.24hnhadat.vn//"+document.location.pathname+"</a>";
     var copytext = pagelink;
     var newdiv = document.createElement('div');
     newdiv.style.position='absolute';
